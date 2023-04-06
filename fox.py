bb8d522f533f65af8a305883b39c178615db5e52
Private-053 <hugojbrandao@gmail.com>
2023-04-07 00:37:31 +0100

    completo

--- fox.py
+++ fox.py
@@ -3,55 +3,59 @@ import requests
 from bs4 import BeautifulSoup
 
 
+class Fox():
 
-#get info trough the URL
+    def __init__(self):
+        self.dic = {}
+        self.channels = ["foxcomedy", "fox-crime", "foxlife", "fox-movies", "mundo-fox", "foxtv"]
 
 
-#get info from FOX
+    def get_fox(self, date, channel):
+        if self.dic.get(date) is None or self.dic.get(date).get(channel) is None:
 
-def get_fox(channel):
-    #remove the - from data
-    fox = {}
-    date = str(get_date()).replace("-", "")
-    url = "https://www.foxtv.pt/programacao/" + channel + "/" + date + "#day" + date
-    print(url)
-    r = requests.get(url)
-    data = r.text
-    #write html
-    with open("fox.html", "w") as f:
-        f.write(data)
+            #remove the - from data
+            fox = {}
+            data = date.replace("-", "")
+            url = "https://www.foxtv.pt/programacao/" + channel + "/" + data + "#day" + data
+            print(url)
+            r = requests.get(url)
+            data = r.text
+            #write html
+            with open("fox.html", "w") as f:
+                f.write(data)
 
-    soup = BeautifulSoup(data, "html.parser")
+            soup = BeautifulSoup(data, "html.parser")
 
+            
+            fox['programa'] = (soup.find_all("div", class_="large-8 medium-8 small-8 column"))
+            fox['hora'] = (soup.find_all("div", class_="large-3 medium-3 small-3 column"))
+            fox['dia'] = (soup.find_all("li", class_="acilia-schedule-event"))
+
+            
+
+            #clean data
+            for i in range(len(fox['programa'])):
+                fox['programa'][i] = fox['programa'][i].find("h3").text
+                fox['hora'][i] = fox['hora'][i].text
+                fox['dia'][i] = fox['dia'][i].get("data-datetime-date").replace("-", "")
+
+
+            if self.dic.get(date) is None:
+                self.dic[date] = {}
+            
+            self.dic[date][channel] = fox
+
+            #write to file
+            with open("fox.txt", "w") as f:
+                for i in range(len(fox['programa'])):
+                    f.write(fox['programa'][i] + " " + fox['hora'][i]  + " " + fox['dia'][i] + "\n")
+
+
+
+
+        return self.dic
     
-    fox['programa'] = (soup.find_all("div", class_="large-8 medium-8 small-8 column"))
-    fox['hora'] = (soup.find_all("div", class_="large-3 medium-3 small-3 column"))
-    fox['dia'] = (soup.find_all("li", class_="acilia-schedule-event"))
 
-
-
-
-    
-    #clean data
-    for i in range(len(fox['programa'])):
-        fox['programa'][i] = fox['programa'][i].find("h3").text
-        fox['hora'][i] = fox['hora'][i].text
-        fox['dia'][i] = fox['dia'][i].get("data-datetime-date").replace("-", "")
-
-    #write to file
-    with open("fox.txt", "w") as f:
-        for i in range(len(fox['programa'])):
-            f.write(fox['programa'][i] + " " + fox['hora'][i]  + " " + fox['dia'][i] + "\n")
-
-
-
-
-    return fox
-
-#get today's date
-
-def get_date():
-    today = datetime.date.today()
-    return today
-
-get_fox("foxcomedy")
+if __name__ == "__main__":
+    fox = Fox()
+    fox.get_fox("2023-04-08", "foxcomedy")
--- tvcine.py
+++ tvcine.py
@@ -1,51 +1,55 @@
 import requests
 import datetime
 
+class TVCine:
 
-def get_tvcine(channels):
-    tvcine = {}
-    tvcine['programa'] = []
-    tvcine['hora'] = []
-    tvcine['dia'] = []
-    semana = get_date() + datetime.timedelta(days=7)
-    semana = str(semana)
-    date = str(get_date())
-    url = "https://api-tvcine.com/content/emissoes/range?dateStart=" + date + "%2000:00&dateEnd=" + semana + "%2023:59&timezone=Europe/Lisbon"
-    print(url)
-    r = requests.get(url)
-    data = r.json()
+    def __init__(self):
+        self.dic = {}
+        #self.channels = ["TVC Top", "TVC Emotion", "TVC Edition", "TVC Action"]
+
+    def get_tvcine(self, date, channels):
+        if self.dic.get(date) is None:
+
+            tvcine = {}
+            tvcine['programa'] = []
+            tvcine['hora'] = []
+            tvcine['dia'] = []
+            url = "https://api-tvcine.com/content/emissoes/range?dateStart=" + date + "%2000:00&dateEnd=" + date + "%2023:59&timezone=Europe/Lisbon"
+            print(url)
+            r = requests.get(url)
+            data = r.json()
+            
+
+            #split by channel
+            data.sort(key=lambda x: x['canal'])
+
+                #write in file
+            with open("tvcine.json", "w") as f:
+                f.write(str(data))
+
+            #get channel
+            for i in range(len(data)):
+                if data[i]['canal'] in channels:
+                    for e in data[i]['emissoes']:
+                        tvcine['programa'].append(e['tituloPT'])
+                        tvcine['hora'].append(e['horaEmissao'].replace("h", "."))
+                        tvcine['dia'].append(e['dataEmissao'].split("T")[0])
+                        if self.dic.get(date) is None:
+                            self.dic[date] = {}
+                        self.dic[date][data[i]['canal']] = tvcine
+
+            #write to file
+            with open("tvcine.txt", "w") as f:
+                for i in range(len(tvcine['programa'])):
+                    f.write(tvcine['programa'][i] + " " + tvcine['hora'][i]  + " " + tvcine['dia'][i] + "\n")
+
+
+        return self.dic
     
-
-
-    #split by channel
-    data.sort(key=lambda x: x['canal'])
-
-
-        #write in file
-    with open("tvcine.json", "w") as f:
-        f.write(str(data))
-
-    #get channel
-    print(len(data))
-    for i in range(len(data)):
-        if data[i]['canal'] in channels:
-            for e in data[i]['emissoes']:
-                tvcine['programa'].append(e['tituloPT'])
-                tvcine['hora'].append(e['horaEmissao'].replace("h", "."))
-                tvcine['dia'].append(e['dataEmissao'].split("T")[0])
-
-
-    #write to file
-    with open("tvcine.txt", "w") as f:
-        for i in range(len(tvcine['programa'])):
-            f.write(tvcine['programa'][i] + " " + tvcine['hora'][i]  + " " + tvcine['dia'][i] + "\n")
+if __name__ == "__main__":
+    tvcine = TVCine()
+    channels = ["TVC Top", "TVC Emotion", "TVC Edition", "TVC Action"]
+    tvcine.get_tvcine("2023-04-08", channels)
 
 
 
-
-def get_date():
-    today = datetime.date.today()
-    return today
-
-
-get_tvcine(("TVC Emotion", "TVC Action", "TVC Edition", "TVC Top"))
