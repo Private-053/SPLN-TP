cab96787b280e6636522f8408c86e4b96776dab6
danielfaria89 <danielfaria089@gmail.com>
2023-04-07 18:26:32 +0100

    AMC Schedule voltou a funcionar, agora com selenium

--- AmcSchedule.py
+++ AmcSchedule.py
@@ -1,5 +1,10 @@
 import json
-import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 class AmcSchedule():
     def __init__(self):
@@ -8,13 +13,37 @@ class AmcSchedule():
     def get_schedule(self,data):
         if(self.schedule.get(data) is None):
             programacao=[]
-            link=f"https://www.amctv.pt/wp/wp-admin/admin-ajax.php"
-            payload = {"action":"schedule-epg", "start-date":data, "days":1, "tz":"Europe/Lisbon", "channel":"AMC Portugal", "show-title":None, "security":"9a0bf10486"}
-            session = requests.Session()
-            r = session.post(link, data=payload)
-            schedule=r.json()["schedule"][data]
-            for i in schedule:
-                new_entry = {"programa":i["title"],"hora":i["runtime"],"dia":data}
-                programacao.append(new_entry)
-            self.schedule[data]=programacao
+            link=f"https://www.amctv.pt/programacao/"
+            #Run in headless mode
+            options = webdriver.ChromeOptions()
+            options.add_argument('headless')
+            driver = webdriver.Chrome(options=options)
+            try:
+                driver.get(link)
+                #WAIT FOR ren-schedule-items__day TO BE PRESENT
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "ren-schedule-items__day")))
+
+                content=driver.page_source
+
+                
+
+                soup = BeautifulSoup(content, 'html.parser')
+
+                #YYYY-MM-DD to MM/DD/YYYY
+
+                dataNew=data.split("-")[1]+"/"+data.split("-")[2]+"/"+data.split("-")[0]
+
+                scheduleDia=soup.find("div",{"class":"ren-schedule-items__day","data-day":dataNew})
+
+                for i in scheduleDia.find_all("article",{"class":"ren-schedule-item"}):
+                    hora=i.find("div",{"class":"ren-schedule-item__time"}).text
+                    nome=i.find("div",{"class":"ren-schedule-item__series-name"}).text
+                    new_entry = {"programa":nome,"hora":hora,"dia":data}
+                    programacao.append(new_entry)
+                self.schedule[data]=programacao
+
+            except Exception as e:
+                print("Error getting schedule for channel AMC on date "+data,e)
+                return []
+            
         return self.schedule[data]
