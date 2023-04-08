c1900f3daeee68818a548346ac53fca728f4b949
Private-053 <hugojbrandao@gmail.com>
2023-04-08 16:48:36 +0100

    trabalho quase concluido

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -5,13 +5,13 @@ const axios = require('axios');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   axios.get('http://localhost:5000/')
-    .then(function (response) {
-      res.render('index', {data: response.data });
+    .then(response => {
+      dados = response.data;
+      res.render('index', {programacao: dados });
     })
     .catch(function (error) {
       console.log(error);
     })
-
 });
 
 module.exports = router;
