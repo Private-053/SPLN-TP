e547bb26aec3aa86d1bd56a60cf2ed86a7bcaf3f
Grodd074 <a93289@alunos.uminho.pt>
2023-04-09 16:49:39 +0100

    reactivar os outros canais

--- backend/app.py
+++ backend/app.py
@@ -26,11 +26,11 @@ def index():
     if dia is None:
         dia=date.today().strftime("%Y-%m-%d")
 
-    #schedule.update(axn.get_all_schedules(dia))
-    #schedule["amc"]=amc.get_schedule(dia)
-    #schedule.update(fox.get_all_schedules(dia))
+    schedule.update(axn.get_all_schedules(dia))
+    schedule["amc"]=amc.get_schedule(dia)
+    schedule.update(fox.get_all_schedules(dia))
     schedule["hollywood"]=hollywood.get_schedule(dia)
-    #schedule.update(tvcine.get_all_schedules(dia))
+    schedule.update(tvcine.get_all_schedules(dia))
 
     return jsonify(schedule)
 
